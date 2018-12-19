c25c3e4028bd322890d5df08d760dbe431fb416d
Jakub Sobolewski <jakupsob@gmail.com>
2018-12-19 18:37:54 +0100

    Achieved circa 50fps

--- unlimitedGliding.py
+++ unlimitedGliding.py
@@ -44,23 +44,21 @@ class Simulation:
 		dY = np.random.randint(low=0, high=self.max_move + 1,size=self.n_drones)
 		uniform = np.random.random(size=self.n_drones)
 		is_in_rectangle = lambda drone, dx, dy, bound, thermal: (((drone[1] + dx) % bound) >= thermal[1] or ((drone[1] - dx) % bound) <= thermal[1]) and (((drone[0] + dy) % bound) >= thermal[0] or ((drone[0] - dy) % bound) <= thermal[0])	
-		for d,drone in enumerate(self.drones):
+		for d in range(self.n_drones):
 			if uniform[d] < np.exp(-max(dX[d],dY[d])):		
-				self.drones[d][0] = (drone[0] + dY[d]) % self.n
-				self.drones[d][1] = (drone[1] + dX[d]) % self.n
-				for thermal in self.thermals:
-					if drone[1] == thermal[1] and drone[0] == thermal[0]:
-						self.known_thermals[d][1] = thermal[1]
-						self.known_thermals[d][0] = thermal[0]
-						break
+				self.drones[d][0] = (self.drones[d][0] + dY[d]) % self.n
+				self.drones[d][1] = (self.drones[d][1] + dX[d]) % self.n
+				if self.base_img[self.drones[d][0],self.drones[d][1]][0] == 255:
+					self.known_thermals[d][1] = self.drones[d][1]
+					self.known_thermals[d][0] = self.drones[d][0]
 				else:
-					can_go = [thermal for thermal in self.known_thermals if is_in_rectangle(drone,dX[d],dY[d],self.n,thermal)]
+					can_go = [[(self.drones[d][0]+dy)%self.n,(self.drones[d][1]+dx)%self.n] for dx in range(-dX[d],dX[d]+1) for dy in range(-dY[d],dY[d]+1) if self.drones_img[(self.drones[d][0]+dy)%self.n,(self.drones[d][1]+dx)%self.n][2] == 255]
 					thermal = can_go[np.random.randint(0,len(can_go))]
 					self.drones[d][1] = thermal[1]
 					self.drones[d][0] = thermal[0]		
 					self.known_thermals[d][1] = thermal[1]
 					self.known_thermals[d][0] = thermal[0]
-	
+					
 	def loop(self):
 		fig = plt.figure(figsize=(12, 12))
 		ax = fig.gca()
@@ -76,17 +74,17 @@ class Simulation:
 			
 		self.im = plt.imshow(self.base_img,animated=True)
 
+		self.start = time.time()
 		_ = FuncAnimation(fig, self.update, interval=0, blit=True)
 		plt.show()
 			
 	def update(self,frame_number):
-		mask = np.zeros(self.base_img.shape,dtype=np.uint8)
-		mask[tuple(self.drones.T)] = (0,0,255,255)
-		mask = self.base_img + mask
-		self.im.set_data(mask)
+		self.drones_img = np.zeros(self.base_img.shape,dtype=np.uint8)
+		self.drones_img[tuple(self.drones.T)] = (0,0,255,255)
+		self.im.set_data(self.drones_img + self.base_img)
 		self.move_drones()
 		self.iterate = self.iterate + 1
-		self.time_text.set_text("{:d}".format(self.iterate))
+		self.time_text.set_text("fps={:f}".format(self.iterate/(time.time()-self.start)))
 		return [self.im, self.time_text]
 	
 if __name__ == '__main__':
